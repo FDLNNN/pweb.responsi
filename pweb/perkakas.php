16c0a24c940090c4b5f43124d87a20753c6dfb65
FDLNNN <101852832+FDLNNN@users.noreply.github.com>
2022-07-21 00:48:40 +0700

    Add files via upload

---
+++ pweb/perkakas.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PERKAKAS|STORE</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+    <!-- header -->
+<header>
+    <div class="container">
+        <h1><a>STORE</a></h1>
+        <ul>
+            <li><a href="home.php">HOME</a></li>
+            <li><a href="perkakas.php">PERKAKAS</a></li>
+            <li><a href="jasa.php">JASA</a></li>
+        </ul>
+
+
+</div>
+</header>
+  <body>
+
+  <!-- Menu -->
+  <div class="container">
+    <div class="row mt-3">
+      <div class="col-md-3">
+        <div class="card border-dark">
+          <img src="foto/jual1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">Bor</h5>
+           <label class="card-text harga">Rp.150.000</label><br>
+           <a href="keranjang.php" class="btn btn-success btn-sm">BELI</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-dark">
+          <img src="foto/jual2.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">Palu</h5>
+           <label class="card-text">Rp. 35.000</label><br>
+           <a href="keranjang.php" class="btn btn-success btn-sm">BELI</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-dark">
+          <img src="foto/jual3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">Gergaji mesin</h5>
+            <label class="card-text">Rp. 200.000</label><br>
+            <a href="keranjang.php" class="btn btn-success btn-sm">BELI</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-dark">
+          <img src="foto/jual4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">Baut</h5>
+            <label class="card-text">Rp. 25.000</label><br>
+            <a href="keranjang.php" class="btn btn-success btn-sm">BELI</a>
+          </div>
+        </div>
+      </div>
+     </div>  
+     <div class="row mt-3">
+      <div class="col-md-3">
+        <div class="card border-dark">
+          <img src="foto/jual5.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">moer</h5>
+            <label class="card-text">Rp. 15.000</label><br>
+            <a href="keranjang.php" class="btn btn-success btn-sm">BELI</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-dark ">
+          <img src="foto/jual6.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">oObeng</h5>
+            <label class="card-text">Rp. 20.000</label><br>
+            <a href="keranjang.php" class="btn btn-success btn-sm">BELI</a>
+          </div>
+        </div>
+      </div>        
+      <div class="col-md-3">
+        <div class="card border-dark">
+          <img src="foto/jual7.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">Kunci L</h5>
+            <label class="card-text">Rp. 50.000</label><br>
+            <a href="keranjang.php" class="btn btn-success btn-sm">BELI</a>
+          </div>
+        </div>
+      </div> 
+      <div class="col-md-3">
+        <div class="card border-dark">
+          <img src="foto/jual8.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">Linggis</h5>
+            <label class="card-text">Rp. 49.000</label><br>
+            <a href="keranjang.php" class="btn btn-success btn-sm">BELI</a>
+          </div>  
+     </div> 
+  </div>
+  <!-- Akhir Menu -->
+   <!-- Awal Footer -->
+   <hr class="footer">
+   <div class="container">
+     <div class="row footer-body">
+       <div class="col-md-6">
+       <div class="copyright">
+         <strong>UCUP COPYRIGHT</strong> <i class="ZARANS-copyright"></i> 2022</p>
+       </div>
+       </div>
+     </div>
+   </div>
+<!-- Akhir Footer -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+</html>
